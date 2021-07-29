671504754a932785e0ff748a85df606422cd01cf
PawelBis <bis1pawel1@gmail.com>
2021-07-29 21:47:40 +0200

    Org improvements, Some evil mode stuff

--- init.el
+++ init.el
@@ -10,6 +10,8 @@
 (load-theme 'tango-dark)
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
+(set-frame-font "Consolas 12" nil t)
+
 (column-number-mode)
 (global-display-line-numbers-mode t)
 (dolist (mode '(org-mode-hook
@@ -19,8 +21,8 @@
 
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+			 ("org" . "https://orgmode.org/elpa/")))
+
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -83,15 +85,23 @@
   (load-theme 'doom-dracula t)
   (doom-themes-org-config))
 
+(use-package hydra)
+(defhydra hydra-text-scale (:timeout 3)
+  "scale text"
+  ("j" text-scale-increase "increase")
+  ("k" text-scale-decrease "decrease")
+  ("q" nil "quit" :exit t))
 
 (use-package general
 :config
   (general-create-definer rune/leader-keys
     :keymaps '(normal insert visual emacs)
     :prefix "SPC"
-    :global-prefix "C-SPC")
-  (rune/leader-keys
-    "tt" '(counsel-load-theme :which-key "choose theme")))
+    :global-prefix "C-SPC"))
+
+(rune/leader-keys
+  "tt" '(counsel-load-theme :which-key "choose theme"))
+
 
 (defun rune/evil-hook ()
   (dolist (mode '(custom-mode
@@ -122,16 +132,65 @@
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
 
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/projects/")
+	   (setq projectile-project-search-path '("~/projects/")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :init (counsel-projectile-mode))
+
+(use-package magit
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-funciton #'magit-display-buffer-same-window-except-diff-v1))
+
+(defun dw/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil))
+
+(use-package org
+  :hook (org-mode . dw/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+	org-hide-emphasis-markers t))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(with-eval-after-load 'org-faces
+(dolist (face '((org-level-1 . 1.2)
+		(org-level-2 . 1.1)
+		(org-level-3 . 1.05)
+		(org-level-4 . 1.0)))
+  (set-face-attribute (car face) nil :font "Consolas" :weight 'regular :height (cdr face))))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
+ '(package-selected-packages
+   '(evil general doom-themes doom-modeline rainbow-delimiters which-key ivy-rich counsel ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
